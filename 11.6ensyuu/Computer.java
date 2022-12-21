158a39dc9a33f96bad99e5400081f6cccfd37e13
EC2 Default User <se.eguti0824@gmail.com>
2022-12-21 14:01:07 +0000

    [add file]抽象クラス

--- 11.6ensyuu/Computer.java
+++ 11.6ensyuu/Computer.java
@@ -1,1 +1,11 @@
-public class Computer extends TangibleAsset
+// クラス定義（継承）
+public class Computer extends TangibleAsset{
+ String makerName;
+  // コンストラクタ
+  public book(String name,int price, String color,String makerName){
+    super(name,price,color);
+    this.makerName = makerName;
+  }
+  
+  public String getMakerName(){return this.makerName};
+}
